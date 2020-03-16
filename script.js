73eae4b113941142fb7fc8f2454b09eff18568bf
Baptiste Bonnin <baptiste.bonnin@ynov.com>
2020-03-16 16:00:26 +0100

    maj

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeLight ( flag )
+{
+    if ( flag )
+    { // light mode
+        document.getElementById ( 'lightCss' ).href = '/script.css';
+    }
+    else
+    { // dark mode
+        document.getElementById ( 'lightCss' ).href = '/script.css';
+    }
+}
