f520a90f01bffc359f86cbde72cdd59060b84ece
Biozard01 <arthur.laforest33@gmail.com>
2020-03-24 14:07:19 +0100

    up

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function changeLight ( flag )
-{
-    if ( flag )
-    { // light mode
-        document.getElementById ( 'lightCss' ).href = '/script.css';
-    }
-    else
-    { // dark mode
-        document.getElementById ( 'lightCss' ).href = '/script.css';
+function changeLight(flag) {
+    if (flag) { // light mode
+        document.getElementById('onlyLight').href = '/style.css';
+    } else { // dark mode
+        document.getElementById('onlyDark').href = '/style.css';
     }
 }
