fe683fdc976856d1be9c4f982b61ed41e734f4f4
Biozard01 <arthur.laforest33@gmail.com>
2020-04-07 14:36:38 +0200

    oh no

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeLight(flag) {
-    if (flag) { // light mode
-        document.getElementById('onlyLight').href = '/style.css';
-    } else { // dark mode
-        document.getElementById('onlyDark').href = '/style.css';
-    }
-}
