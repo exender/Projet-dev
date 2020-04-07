6613cb4f48ae72642376cdec2cd77af47743ffda
Biozard01 <arthur.laforest33@gmail.com>
2020-04-07 18:14:08 +0200

    swag

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function summonerLookUp() {
     var SUMMONER_NAME = "";
     SUMMONER_NAME = $("#userName").val();
 
-    var API_KEY = "RGAPI-c56bb311-f74e-4901-bde1-03249ac3575f";
+    var API_KEY = '?api_key=' + "RGAPI-c56bb311-f74e-4901-bde1-03249ac3575f";
 
     if (SUMMONER_NAME !== "") {
 
         $.ajax({
-            url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + SUMMONER_NAME + '?api_key=' + API_KEY,
+            url: 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + SUMMONER_NAME + API_KEY,
             type: 'GET',
             dataType: 'json',
             data: {
@@ -21,15 +21,15 @@ function summonerLookUp() {
                 SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
 
                 summonerLevel = json[SUMMONER_NAME_NOSPACES].summonerLevel;
-                summonerID = json[SUMMONER_NAME_NOSPACES].id;
+                summonerName = json[SUMMONER_NAME_NOSPACES].name;
 
                 document.getElementById("sLevel").innerHTML = summonerLevel;
-                document.getElementById("sID").innerHTML = summonerID;
+                document.getElementById("sName").innerHTML = summonerName;
 
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 alert("error getting Summoner data!");
             }
         });
-    } else {}
+    }
 }
